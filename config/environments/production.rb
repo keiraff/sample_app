be0b9b07f3ced3ee71b12eb6ed461b11e88b013b
Diana <diana.hlomko@gmail.com>
2021-12-07 16:45:10 +0300

    Fix sending verification emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,18 +62,32 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_production"
 
-  config.action_mailer.perform_caching = false
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.perform_caching = false
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # host = 'https://glacial-journey-90957.herokuapp.com'
+  # config.action_mailer.default_url_options = { host: host }
+  # ActionMailer::Base.smtp_settings = {
+  #   :address        => 'smtp.sendgrid.net',
+  #   :port           => '587',
+  #   :authentication => :plain,
+  #   :user_name      => ENV['SENDGRID_USERNAME'],
+  #   :password       => ENV['SENDGRID_PASSWORD'],
+  #   :domain         => 'heroku.com',
+  #   :enable_starttls_auto => true
+  # }
+
   config.action_mailer.delivery_method = :smtp
-  host = 'https://glacial-journey-90957.herokuapp.com'
+  host = 'https://glacial-journey-90957.herokuapp.com/' #replace with your own url
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'olgadianajulik@gmail.com',
+    :password             => 'OlgaDianaJulik123!',
+    :authentication       => "plain",
     :enable_starttls_auto => true
   }
 
